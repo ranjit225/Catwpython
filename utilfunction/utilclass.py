9dd717a46a36fb200d68fb136956e4f1d7c3334d
rnarravula <ranjitn96@gmail.com>
2020-05-28 13:07:43 +0530

    python framework-URL changed

--- utilfunction/utilclass.py
+++ utilfunction/utilclass.py
@@ -3,5 +3,5 @@ class functionutil(object):
     def __init__(self,driver):
         self.driver=driver
     def ScreenShot(self,path):
-        directory="C:/Users/rnarravula/PycharmProjects/Catwautomation/Screenshots"
+        directory="C:/Users/rnarravula/PycharmProjects/Catwpython/Screenshots"
         self.driver.get_screenshot_as_file(directory+path)
